a536d2840ab39377015e5cc93033091d1003b624
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-16 13:11:59 -0300

    ajsutando o caminho da imagem2

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
   <!-- Imagem de destaque -->
   <div class="container mt-4">
-    <img src="img/principalIndex.avif​" alt="Imagem sobre finanças" class="hero-image">
+    <img src="img/principalIndex.jpg" alt="Imagem sobre finanças" class="hero-image">
     
     <!-- Texto inspirador -->
     <div class="hero-text">
