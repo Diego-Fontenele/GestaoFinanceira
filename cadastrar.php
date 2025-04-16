06c4e1e14d87ead317ccdebf1e1a492245b33fbf
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-16 17:24:05 -0300

    ajuste na tela de cadatrar usuario erro na conexao

--- cadastrar.php
+++ cadastrar.php
@@ -5,7 +5,7 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
-$stmt = $conn->prepare($sql);
+$stmt = $pdo->prepare($sql);
 $stmt->bindParam(':nome', $nome);
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':senha', $senha);
