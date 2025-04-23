faa500b1132a8d7cbb9858aeef88349b0ae460c5
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-23 10:01:31 -0300

    Finalizando a tela de inserção investimento e movimentação de investimento

--- add_investimento.php
+++ add_investimento.php
@@ -158,6 +158,7 @@ $investimentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.8/dist/inputmask.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.8/dist/bindings/inputmask.binding.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
   function excluirInvestimento(id) {
   Swal.fire({
