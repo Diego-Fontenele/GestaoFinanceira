297402d311508980cec6e6e422c15fd07026aed1
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-23 18:13:01 -0300

    ajustando area logada

--- area_logada.php
+++ area_logada.php
@@ -155,6 +155,16 @@ $valorMeta = $valoresMeta[$primeiraMetaTitulo] ?? 0;
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+
+  .full-height {
+    min-height: 100vh;
+  }
+</style>
 </head>
 <body style="background-color: #f8f9fa;" class="min-vh-100">
 
