ad255a035a1e3f109e57f490279fcf9357c86a05
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-23 09:50:44 -0300

    Finalizando a tela de inserção investimento e movimentação de investimento

--- add_investimento.php
+++ add_investimento.php
@@ -71,6 +71,7 @@ $investimentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <meta charset="UTF-8">
   <title>Novo Investimento</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body class="bg-light">
