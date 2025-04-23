038b2b28a24a1f72b40b5b3c21b1ce0ac1e6012c
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-23 18:06:12 -0300

    ajustando area logada

--- fale_conosco.php
+++ fale_conosco.php
@@ -48,6 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <title>Fale Conosco</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 
