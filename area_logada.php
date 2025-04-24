a4a17ab341f655f1a2406195469a71ee73db8da8
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-24 12:09:28 -0300

    ajustando a área logada

--- area_logada.php
+++ area_logada.php
@@ -216,7 +216,7 @@ $valorMeta = $valoresMeta[$primeiraMetaTitulo] ?? 0;
       <div class="card-body" style="max-height: 400px; overflow-x: auto; overflow-y: hidden;">
         <h5 class="card-title mb-3"><i class="bi bi-graph-down-arrow"></i> Evolução das Despesas</h5>
         <div style="width: 100%; max-width: 1200px; overflow-x: auto; overflow-y: hidden; border: 1px solid #ccc; padding: 10px;">
-          <div style="width: 1200px; height: 300px;">
+          <div style="width: 1200px; height: 200px;">
             <canvas id="graficoDespesasMes" class="w-100" style="height: 100%;"></canvas>
           </div>
         </div>
