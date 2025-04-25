b0fed931388b3333c43bf2eb1f8506a69d831be7
Diego-Fontenele <diegocfontenele@gmail.com>
2025-04-25 11:11:33 -0300

    ajustando a tela de envio de e-mail

--- fale_conosco.php
+++ fale_conosco.php
@@ -55,6 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>Fale Conosco</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   
 </head>
 <body class="bg-light">
