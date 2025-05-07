c7974e3f9f3fa3f3f7fb1956ac7060ccade12528
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-07 17:50:20 -0300

    ajustando menu area logada

--- mentor_dashboard.php
+++ mentor_dashboard.php
@@ -116,6 +116,12 @@ $metasAluno = $sqlMetasAluno->fetchAll(PDO::FETCH_ASSOC);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+</style>
 </head>
 <body style="background-color: #f8f9fa;">
 
