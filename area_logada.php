0f334c8a7b1fca865dbc5993e9d6fa0733069cf5
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-07 16:08:45 -0300

    ajustando menu area logada

--- area_logada.php
+++ area_logada.php
@@ -251,7 +251,7 @@ $valorMeta = $valoresMeta[$primeiraMetaTitulo] ?? 0;
   <div class="col-md-6 mb-4 d-flex">
     <div class="card w-100 h-100">
       <div class="card-body">
-        <h5 class="card-title mb-3 d-flex justify-content-between align-items-center"><i class="bi bi-list-ul"></i> Despesas por Descrição (Top 10)
+        <h5 class="card-title mb-3 d-flex justify-content-between align-items-center"><span><i class="bi bi-list-ul"></i> Despesas por Descrição (Top 10)</span>
           <form  id="formFiltroMes" method="GET" class="mb-0">
             <input type="month" name="mes_descricao" class="form-control form-control-sm" style="width: 150px;" value="<?= $mesSelecionado  ?>">
           </form>
