e40e418f2a7729f80d03d334f3d7d9bb49f2efaf
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-07 14:48:21 -0300

    ajustando menu area logada

--- area_logada.php
+++ area_logada.php
@@ -194,7 +194,7 @@ $valorMeta = $valoresMeta[$primeiraMetaTitulo] ?? 0;
 </head>
 <body class="bg-light">
 
-<div class="d-flex">
+<div class="d-flex"  style="width: 250px;" >
   <!-- Inclusão do menu lateral -->
   <?php include('includes/menu.php'); ?>
  
