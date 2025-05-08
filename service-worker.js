a09891804fa5b025c70a652f738b62b44d6dddac
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-08 18:58:39 -0300

    gerando o PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app-cache-v5"; // Atualize a versão quando mudar arquivos
+const CACHE_NAME = "app-cache-v6"; // Atualize a versão quando mudar arquivos
 
 const URLS_TO_CACHE = [
   "/index.php",
