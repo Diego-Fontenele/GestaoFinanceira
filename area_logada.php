4c0cbc93a09b18563906f64695e5ccf25c5b62f2
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-08 11:12:56 -0300

    Criando mais um gráfico de total de metas e seu percentual de conclusão

--- area_logada.php
+++ area_logada.php
@@ -327,7 +327,7 @@ $metas = $stmt->fetchAll(PDO::FETCH_ASSOC);
               </form>
             </h5>
             <div style="width: 100%; max-width: 1200px; overflow-x: auto; overflow-y: hidden; border: 1px solid #ccc; padding: 10px;">
-              <div style="width: 1200px; height: 300px;">
+              <div style="width: 1100px; height: 300px;">
                 <!-- Ajustando o gráfico de progresso para ter altura fixa -->
                 <canvas id="graficoProgressoMeta" class="w-100" style="height: 100%;"></canvas>
               </div>
