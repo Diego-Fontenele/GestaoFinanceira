a150573563956f1d7cf8eee443bf45302f7dbeb1
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-12 15:29:38 -0300

    utilizando o dominio e e-mail do uol

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta name="theme-color" content="#0d6efd">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gestão Financeira</title>
+  <title>Domine Seu Bolso</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     .hero-image {
