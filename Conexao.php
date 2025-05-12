bb96e02c918d425d1e09c591fff55eee9f35659d
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-12 19:31:23 -0300

    mudando o servidor de banco de dados

--- Conexao.php
+++ Conexao.php
@@ -1,15 +1,17 @@
 <?php
-$host = 'dpg-cvvtr6be5dus73cl1f6g-a';  // O host fornecido pelo Render
-$dbname = 'projetofinanceiro';             // O nome do banco de dados
-$username = 'projetofinanceiro_user';         // O usuário fornecido pelo Render
-$password = 'eHvNKFJAjsAGuMXaXHh2L9TIy34BED70';           // A senha fornecida pelo Render
+$host = 'ep-red-star-acyn9osu-pooler.sa-east-1.aws.neon.tech';  // O host fornecido pelo neon
+$dbname = 'neondb';             // O nome do banco de dados
+$username = 'neondb_owner';         // O usuário fornecido pelo Render
+$password = 'npg_pbWiu9U7YknO';           // A senha fornecida pelo neon
 
 try {
     // Conexão com o PostgreSQL usando PDO
     $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("SET search_path TO public;");
     //echo "Conectado com sucesso ao PostgreSQL!";
 } catch (PDOException $e) {
     echo "Erro na conexão: " . $e->getMessage();
 }
+
 ?>
