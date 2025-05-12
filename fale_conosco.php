f37786e65c52d3df9b2bf4aba573a2827d15c05b
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-12 14:31:46 -0300

    utilizando o dominio e e-mail do uol

--- fale_conosco.php
+++ fale_conosco.php
@@ -29,9 +29,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $assunto  = $_POST['assunto'] ?? '';
         $mensagem = $_POST['mensagem'] ?? '';
 
-        $mail->setFrom($mail->Username, 'Fale Conosco');
-        $mail->addReplyTo($email, $nome);
-        $mail->addAddress('diegocfontenele@gmail.com', 'Diego');
+        $mail->setFrom($email, $nome );
+      
+        $mail->addAddress($mail->Username, 'Diego');
 
         $mail->isHTML(false);
         $mail->Subject = "Fale Conosco - $assunto";
