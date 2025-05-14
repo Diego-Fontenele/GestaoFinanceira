4731dcb6161bd8b0d8296c6c63528932c7862218
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-14 19:06:25 -0300

    criando o orçamento

--- orcamento.php
+++ orcamento.php
@@ -93,6 +93,7 @@ if (isset($_GET['mes_ano']) && !empty($_GET['mes_ano'])) {
     <meta charset="UTF-8">
     <title>Orçamento por Categoria</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
