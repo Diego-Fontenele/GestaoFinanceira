8c4cfd3f011232f796207a1e8cba7db6d2da2184
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-16 10:03:07 -0300

    Ajustando a tela de esqueci minha senha colocando no padrão novo usuário.

--- esqueceu.php
+++ esqueceu.php
@@ -19,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         // Gera nova senha aleatória
           $novaSenha = bin2hex(random_bytes(4)); // Ex: "a8d3f6e9"
           $hash = password_hash($novaSenha, PASSWORD_DEFAULT);
+          echo "Olá, {$verifica['nome']}<br><br>Sua nova senha é: <strong>{$novaSenha}</strong><br><br>Você pode alterá-la após o login.";
          try {
             $mensagem = "E-mail enviado com sucesso!";
             $resultado = enviarEmail(
