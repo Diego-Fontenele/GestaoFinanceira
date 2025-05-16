0f6f58200d3f06f263dd271139a485766169b49b
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-16 19:11:36 -0300

    criando IA no sistema MENU e TELA

--- mentor_virtual.php
+++ mentor_virtual.php
@@ -43,7 +43,7 @@ if ($mesSelecionado) {
         ];
     
         $data = [
-            'model' => 'gpt-4',
+            'model' => 'gpt-3.5-turbo',
             'messages' => [
                 ['role' => 'user', 'content' => $prompt]
             ],
