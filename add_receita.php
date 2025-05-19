46f90181f9259b2a567f9f5d94e1c198cc2c88f3
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-19 15:15:21 -0300

    criando data de referencia nas receitas

--- add_receita.php
+++ add_receita.php
@@ -106,8 +106,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['excluir_selecionados
         $dataAtual = date('Y-m-d', strtotime("+$i month", strtotime($data)));
         $dataref = date('Y-m-d', strtotime("+$i month", strtotime($datareferencia)));
         var_dump($valor, $dataAtual, $dataref);
-        $stmt = $pdo->prepare("INSERT INTO receitas (usuario_id, categoria_id, descricao, valor, data, data_referencia) VALUES (?, ?, ?, ?, ?, ?)");
         try {
+        $stmt = $pdo->prepare("INSERT INTO receitas (usuario_id, categoria_id, descricao, valor, data, data_referencia) VALUES (?, ?, ?, ?, ?, ?)");
+        
           $stmt->execute([$_SESSION['usuario_id'], $categoria_id, $descricao, $valor, $dataAtual, $dataref]);
         } catch (PDOException $e) {
           throw new Exception("Erro ao inserir mês {$i}: " . $e->getMessage());
