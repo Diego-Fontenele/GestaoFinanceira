08f398747369ff400b831f6e3f5b3f24f7b61c84
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-20 18:11:32 -0300

    ajustando o layout do sistema para PWA

--- add_receita.php
+++ add_receita.php
@@ -206,7 +206,7 @@ $receitas = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+  
 </head>
 
 <body class="bg-light">
@@ -379,7 +379,7 @@ $receitas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <?php endif; ?>
   </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
 </body>
 
 </html>
