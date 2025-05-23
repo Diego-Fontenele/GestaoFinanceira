ee38844a66cfade3022e0e95de7801fbedb9fa6e
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-23 11:07:04 -0300

    ajustando o gráfico despesas

--- area_logada.php
+++ area_logada.php
@@ -373,7 +373,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             <div class="card-body" style="max-height: 400px; overflow-x: auto; overflow-y: hidden;">
               <h5 class="card-title mb-3"><i class="bi bi-graph-down-arrow"></i> Evolução das Despesas</h5>
               <div style="min-width: 100%; max-width: 1200px; overflow-x: auto; overflow-y: hidden; border: 1px solid #ccc; padding: 10px;">
-                <div style="min-width: 100%; height: 300px;">
+                <div style="width: 400px; height: 300px;">
                   <canvas id="graficoDespesasMes" class="w-100" style="height: 100%; width: auto;"></canvas>
                 </div>
               </div>
