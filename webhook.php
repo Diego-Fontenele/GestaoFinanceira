2b8ffe9c4ed57b0c6da4cec6a11796430e762bd3
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-28 13:21:43 -0300

    Criando webhook para automação por zap

--- webhook.php
+++ webhook.php
@@ -37,8 +37,9 @@ if (preg_match('/^(receita|despesa)\s+([a-zA-ZÀ-ÿ\s]+)\s+(\d+(?:[\.,]\d{1,2})?
 // Função para responder usando a Z-API
 function responder($telefone, $mensagem)
 {
-    $token = '43BD3E198157CE2FD5261629'; // Substitua pelo seu token real
-    $url = 'https://api.z-api.io/instances/3E1E0A1E0F3500FD1B149A25103DD3DD/token/' . $token . '/send-text';
+$token = getenv('ZAPI_TOKEN');
+$instancia = getenv('ZAPI_INSTANCIA');
+$url = "https://api.z-api.io/instances/$instancia/token/$token/send-text";
     $dados = [
         'phone' => $telefone,
         'message' => $mensagem
