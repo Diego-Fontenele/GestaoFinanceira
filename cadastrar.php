8e4dddea5674fc12298d8a24561a5d10285aa04e
Diego-Fontenele <diegocfontenele@gmail.com>
2025-05-29 14:26:21 -0300

    criando o campo de telefone na tela de cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -68,6 +68,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   </style>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/5.0.8/jquery.inputmask.min.js"></script>
+  <script>
+  $(function () {
+    $('#telefone').inputmask('(99) 99999-9999', { clearIncomplete: true });
+  });
+</script>
 </head>
 <body>
 
@@ -109,9 +114,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </div>
 
 </body>
-<script>
-  $(function () {
-    $('#telefone').inputmask('(99) 99999-9999', { clearIncomplete: true });
-  });
-</script>
+
 </html>
